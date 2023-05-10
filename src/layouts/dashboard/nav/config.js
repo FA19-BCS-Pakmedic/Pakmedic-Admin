a2e784c65d6f39b8f7ea0ad185315ae1d325c850
Touqeer Hassan <touqeerhassan76@gmail.com>
2023-05-10 15:03:04 +0500

    added appointment request icon

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -26,16 +26,16 @@ const navConfig = [
     path: '/dashboard/appointment-requests',
     icon: icon('ic_appointment'),
   },
-  {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
-  },
+  // {
+  //   title: 'product',
+  //   path: '/dashboard/products',
+  //   icon: icon('ic_cart'),
+  // },
+  // {
+  //   title: 'blog',
+  //   path: '/dashboard/blog',
+  //   icon: icon('ic_blog'),
+  // },
   {
     title: 'login',
     path: '/login',
